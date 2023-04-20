9e6f0f0a5a7b553b6dfb306ccb1f066532f8e733
obidovbek <obidov.bekzod94@gmail.com>
2023-04-20 10:02:54 +0500

    pull

--- src/scrapper/scrapper.service.ts
+++ src/scrapper/scrapper.service.ts
@@ -7,14 +7,10 @@ export class ScrapperService {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto('https://scholar.google.com/citations?view_op=view_citation&hl=ru&user=itSn6DMAAAAJ&citation_for_view=itSn6DMAAAAJ:9yKSN-GCB0IC');
-    // const f = await page.$('#gsc_oci_title > a')
-    // //obtain text
-    // const text = await (await f.getProperty('textContent')).jsonValue()
-    // console.log("Text is: " + text)
-    const element = await page.waitForSelector('#gsc_oci_title > a'); // select the element
-    const text = await element.evaluate(el => el.textContent); // grab the textCo
-    console.log("Text is: " + text)
+    const element = await (await page.waitForSelector('#gsc_oci_title > a')).evaluate(el => el.textContent); // select the element
+    console.log("Text is: 12" + element)
     await browser.close();
+
     return [];
   }
 }
