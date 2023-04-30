d5e5a7703e590676c21dd58d197874c6802d62b5
obidovbek <obidov.bekzod94@gmail.com>
2023-05-01 04:41:50 +0500

    pull

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ScrapperModule } from './scrapper/scrapper.module';
 @Module({
   imports: [
-    ScrapperModule, 
+    ScrapperModule
   ],
   controllers: [],
   providers: [],
